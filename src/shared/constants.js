10a690510d0374d4bce85b3544b406081ef3001f
angelinadegay <59676843+angelinadegay@users.noreply.github.com>
2022-10-19 15:24:57 -0400

    web-serv constants upgrade for v 0.1

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -13,11 +13,23 @@ module.exports = Object.freeze({
 
   MAP_SIZE: 3000,
   MSG_TYPES: {
-    JOIN_GAME: 'join_game',
+    JOIN_DRIVER: 'join_driver',
+    JOIN_ROBOT: 'join_robot',
     GAME_UPDATE: 'update',
     ROTATE: 'rotate',
     MOVE: 'move',
     INPUT: 'input',
     GAME_OVER: 'dead',
   },
+
+  ARDUINO_MOVEMENT_DIRECTIONS: {
+    FORWARD: 'W',
+    BACKWARD: 'S',
+    RIGHT: 'D',
+    LEFT: 'A',
+    FORWARD_RIGHT: 'E',
+    FORWARD_LEFT: 'Q',
+    BACKWARD_RIGHT: 'C',
+    BACKWARD_LEFT: 'Z',
+  }
 });
